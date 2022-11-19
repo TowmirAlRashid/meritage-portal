7b2a50069053b996a1846f2a09028e53126fed15
TowmirAtInsta <towmir@instawebworks.com.au>
2022-11-19 11:36:36 +0600

    added contact target component

--- components/engagementTabs/contactTarget.jsx
+++ components/engagementTabs/contactTarget.jsx
@@ -1,8 +1,100 @@
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material';
 import React from 'react'
 
 const ContactTarget = () => {
   return (
-    <div>ContactTarget</div>
+    <TableContainer>
+            <Box sx={{
+                    pl: "1.5rem !important",
+                    pr: "1.5rem !important"
+                }}>
+                <Table
+                    aria-labelledby="tableTitle"
+                    
+                >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                Head
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                Head
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                Head
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                Head
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        <TableRow
+                            hover
+                        >
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                        </TableRow>
+
+                        <TableRow
+                            hover
+                        >
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                        </TableRow>
+
+                        <TableRow
+                            hover
+                        >
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                        </TableRow>
+
+                        <TableRow
+                            hover
+                        >
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>
+                                cell
+                            </TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                            <TableCell sx={{ paddingLeft: "1rem"}}>cell</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={5}
+                    // rowsPerPage={rowsPerPage}
+                    // page={page}
+                    // onPageChange={handleChangePage}
+                    // onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Box>
+        </TableContainer>
   )
 }
 
